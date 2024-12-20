d45a105ebaf0218d2a50f23a1ad4eb9652dc8970
Grazielle Bastos <grazielle1101@gmail.com>
2024-12-20 02:03:28 -0300

    edição com flexbox e interação com js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("meuBotao").addEventListener("click", function() {
+  const mensagem = document.getElementById("mensagem");
+  mensagem.style.display = "block"; // Exibe a mensagem
+});
